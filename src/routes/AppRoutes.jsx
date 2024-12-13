b0b4136196b5ed635666cc136a090660a1bc5233
AlejandroVelas23 <velalejandro2333@hotmail.com>
2024-12-13 13:17:23 -0600

    a ver2

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/home/Home';
 import Contact from '../pages/contact/Contact';
-import Nutrition from '../pages/nutrition/nutrition';
+import Nutrition from '../pages/nutrition/Nutrition';
 import Development from '@/pages/development/Development';
 import Architecture from '@/pages/architecture/Architecture';
 import Users from '@/pages/citas/users';
