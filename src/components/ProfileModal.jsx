741014635e9631d5115bfbaff7ac679118a5f042
AlejandroVelas23 <velalejandro2333@hotmail.com>
2024-12-19 16:27:49 -0600

    diseño modal

--- src/components/ProfileModal.jsx
+++ src/components/ProfileModal.jsx
@@ -13,12 +13,12 @@ const ProfileModal = ({ isOpen, onClose }) => {
 
   return (
     <div
-      className={`absolute top-full -right-32 w-64 sm:w-96 bg-gray-950 bg-opacity-90 text-white  shadow-lg transform transition-all duration-300 ease-in-out z-0 ${
+      className={`absolute top-full -right-32 w-64 sm:w-96 bg-gray-950 text-white  shadow-lg transform transition-all duration-300 ease-in-out z-0 ${
         isOpen ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-1 pointer-events-none'
       }`}
     >
       <div className="flex flex-col max-h-[calc(100vh-5rem)] overflow-y-auto">
-        <div className="flex justify-between items-center p-6 border-b sticky top-0 bg-white z-0">
+        <div className="flex justify-between items-center p-6 border-b sticky top-0 bg-gray-950 z-10">
           <h2 className="text-2xl font-semibold text-gray-800">Profile</h2>
           <button
             onClick={onClose}
